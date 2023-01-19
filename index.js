31987f06450283c55d46dd0e941efbeb4c01af59
Diego N <csantana0704@outlook.es>
2023-01-19 11:14:36 -0600

    primer trabajo

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-alert("hola");
-alert("hola3");
-alert("holi");
+    var alumno1 = new Object;
+    alumno1.matricula = 1322134042;
+    alumno1.nombre = "Diego";
+    alumno1.apellido = "Navarro";
+    alumno1.anio_nacimiento = 2004;
+    alumno1.presentacion = function(){
+        let anios = 2023-this.anio_nacimiento;
+        let texto = "hola mi nombre es " + this.nombre + "  "+ this.apellidos + ",  tengo "+ anios +" años";
+        return texto;
+    }
